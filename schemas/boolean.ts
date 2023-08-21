83fb08b35d9827f60c24ba4032028408ab9be7a4
Andres Celis <adwher@icloud.com>
2023-08-21 17:16:45 -0500

    feat(schemas): create `boolean()`

---
+++ schemas/boolean.ts
@@ -0,0 +1,25 @@
+import { SchemaContext } from "../context.ts";
+import { error } from "../errors.ts";
+import { pipeline, SchemaPipes } from "../pipes.ts";
+import { Schema } from "../schema.ts";
+
+export class SchemaBoolean extends Schema<boolean> {
+  constructor(private pipes: SchemaPipes<boolean>) {
+    super();
+  }
+
+  check(value: unknown, context: SchemaContext) {
+    if (typeof value === "boolean") {
+      return pipeline(value, context, this.pipes);
+    }
+
+    return error(context, {
+      message: `Must be a "boolean", got "${typeof value}"`,
+    });
+  }
+}
+
+/** Creates a new `boolean` schema. */
+export function boolean(...pipes: SchemaPipes<boolean>) {
+  return new SchemaBoolean(pipes);
+}
