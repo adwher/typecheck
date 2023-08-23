778aef4e80d6208ae8f1b8b1e36e18f46b5ced00
Andres Celis <adwher@icloud.com>
2023-08-23 17:20:31 -0500

    refactor(schemas): create `SchemaEnumerated`

--- schemas/enumerated.ts
+++ schemas/enumerated.ts
@@ -1,19 +1,57 @@
-import { literal } from "./literal.ts";
-import { union } from "./union.ts";
+import { SchemaContext } from "../context.ts";
+import { error, SchemaError } from "../errors.ts";
+import { Schema } from "../schema.ts";
+import { isNum, isStr } from "../types.ts";
 
 type Enumerable = string | number;
 
+export class SchemaEnumerated<
+  A extends readonly Enumerable[],
+> extends Schema<A[number]> {
+  /** Allowed values of the schema. */
+  private options: Set<Enumerable>;
+
+  /**
+   * Creates a new enumerated schema that only receives the given `options`.
+   * @param options Allowed values of the schema.
+   */
+  constructor(options: A) {
+    super();
+    this.options = new Set(options);
+  }
+
+  check(value: unknown, context: SchemaContext): A[number] | SchemaError {
+    const isEnumerable = isStr(value) || isNum(value);
+
+    if (!isEnumerable) {
+      return error(context, {
+        message: `Must be either "string" or "number", got ${typeof value}`,
+      });
+    }
+
+    if (!this.has(value)) {
+      return error(context, {
+        message: `Must be one of the given options, got "${value}"`,
+      });
+    }
+
+    return value;
+  }
+
+  /** Validate `value` exists in `options`. */
+  has(value: Enumerable) {
+    return this.options.has(value);
+  }
+}
+
 /**
  * Creates a new enumerated schema that only receives the given `options`.
- * This is a alias to use `union` and `literal` to generate the same schema.
  * @example ```
  * const RoleSchema = enumerated("Student", "Teacher", "Administrator");
  * ```
  */
-export function enumerated<E extends Enumerable[]>(...options: E) {
-  const [first, ...rest] = options.map(literal<E[number]>);
-
-  return union(first, ...rest);
+export function enumerated<E extends readonly Enumerable[]>(...options: E) {
+  return new SchemaEnumerated<E>(options);
 }
 
 /** Alias of {@link enumerated}. */
