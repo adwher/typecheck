7ab6b3dd9d111caec02f96ff0ebc9c41c1c95672
Andres Celis <adwher@icloud.com>
2023-08-27 21:52:23 -0500

    feat(utils): create `parse()`

---
+++ utils/parse.ts
@@ -0,0 +1,18 @@
+import { SchemaContext } from "../context.ts";
+import { Schema } from "../schema.ts";
+
+/**
+ * Parse the `value` with the given `schema` and return the result.
+ * @throws An error in cases where the `value` does not satisfies the `schema`.
+ * @returns Parsed `value` with the given `schema`.
+ */
+export function parse<T>(value: unknown, schema: Schema<T>) {
+  const context: SchemaContext = { path: [] };
+  const output = schema.check(value, context);
+
+  if (output instanceof Error) {
+    throw output;
+  }
+
+  return output;
+}
