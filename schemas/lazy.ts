f59c6826a780571d18aec665970562b72c2045ad
Andres Celis <adwher@icloud.com>
2023-09-05 16:57:56 -0500

    chore(schemas): improve lazy generation types

--- schemas/lazy.ts
+++ schemas/lazy.ts
@@ -1,30 +1,30 @@
 import { SchemaContext } from "../context.ts";
 import { SchemaError } from "../errors.ts";
-import { Schema } from "../schema.ts";
+import { Infer, Schema } from "../schema.ts";
 
 /** Generate (lazly) the schema in parsed-time. */
-export type SchemaGenerator<R> = (
+export type SchemaGenerator<S extends Schema> = (
   value: unknown,
   context: SchemaContext,
-) => R | SchemaError;
+) => S | SchemaError;
 
-export class SchemaLazy<R> extends Schema<R> {
+export class SchemaLazy<S extends Schema> extends Schema<Infer<S>> {
   /**
    * Create a new schema that can generate (lazly) the schema in parsed-time.
    * @param getter Generate the right schema on-demand.
    */
-  constructor(private getter: SchemaGenerator<R>) {
+  constructor(private getter: SchemaGenerator<S>) {
     super();
   }
 
-  check(value: unknown, context: SchemaContext): SchemaError | R {
+  check(value: unknown, context: SchemaContext): SchemaError | Infer<S> {
     const output = this.getter(value, context);
 
-    if (output instanceof Schema) {
-      return output.check(value, context);
+    if (output instanceof SchemaError) {
+      return output;
     }
 
-    return output;
+    return output.check(value, context);
   }
 }
 
@@ -34,7 +34,7 @@ export class SchemaLazy<R> extends Schema<R> {
  * Use `SchemaDescribe` and a defined type to use `recursive` schemas.
  * @returns A new `SchemaLazy` with the type of all possible schemas.
  */
-export function lazy<R>(getter: SchemaGenerator<R>) {
+export function lazy<S extends Schema>(getter: SchemaGenerator<S>) {
   return new SchemaLazy(getter);
 }
 
