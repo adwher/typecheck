1e9f628dd845ae0352c69db4847e73d7aebcda60
Andres Celis <adwher@icloud.com>
2023-10-10 22:12:42 -0500

    feat: create `createContext()`

--- context.ts
+++ context.ts
@@ -2,3 +2,8 @@ export interface SchemaContext {
   /** Exact path of the current context. */
   path: Array<string | number>;
 }
+
+/** Create an empty `SchemaContext` ready to use. */
+export function createContext(): SchemaContext {
+  return { path: [] };
+}
