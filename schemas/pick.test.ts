57770d316fd38d78fd4a1f718e54b60693e67c3b
Andres Celis <adwher@icloud.com>
2023-10-13 23:54:25 -0500

    feat(ci): unit-tests for `pick()`

---
+++ schemas/pick.test.ts
@@ -0,0 +1,12 @@
+import { assertObjectMatch } from "std/assert/mod.ts";
+import { object } from "./object.ts";
+import { string } from "./string.ts";
+import { number } from "./number.ts";
+import { pick } from "./pick.ts";
+
+Deno.test("should pick the given fields on the shape", () => {
+  const schema = object({ a: string(), b: number(), c: string() });
+  const omited = pick(schema, ["a", "b"]);
+
+  assertObjectMatch(omited.shape, { a: string(), b: number() });
+});
