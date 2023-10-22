e4818705bb3fda504491c50122f9b7d6b30532e9
Andres Celis <adwher@icloud.com>
2023-10-22 18:24:27 -0500

    feat(ci): unit-tests for `literal()`

---
+++ schemas/literal.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals, assertIsError } from "std/assert/mod.ts";
+
+import { createContext } from "../context.ts";
+import { literal } from "./literal.ts";
+
+const context = createContext();
+const schema = literal("hello");
+
+Deno.test("should pass allowed values", () => {
+  const incorrect = ["bye", 1234, undefined, [], {}];
+
+  assertEquals(schema.check("hello", context), "hello");
+
+  for (const example of incorrect) {
+    assertIsError(schema.check(example, context));
+  }
+});
