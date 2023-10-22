f156e6c8eb3114ed97ad2ff31c162f200c89292d
Andres Celis <adwher@icloud.com>
2023-10-22 17:24:30 -0500

    feat(ci): unit-tests for `unknown()`

---
+++ schemas/unknown.test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { unknown } from "./unknown.ts";
+
+Deno.test("should return the received value", () => {
+  const schema = unknown();
+
+  const correct: unknown[] = [`hello`, `hola`, 1234, false, null, [], {}];
+
+  for (const expected of correct) {
+    assertEquals(schema.check(expected), expected);
+  }
+});
