337a405c719471ebbaa8ea42b9262ff735a61a0f
Andres Celis <adwher@icloud.com>
2023-10-22 18:25:34 -0500

    feat(ci): unit-tests for `optional()`

---
+++ schemas/optional.test.ts
@@ -0,0 +1,21 @@
+import { assertEquals, assertIsError } from "std/assert/mod.ts";
+
+import { createContext } from "../context.ts";
+import { boolean } from "./boolean.ts";
+import { optional } from "./optional.ts";
+
+const context = createContext();
+const schema = optional(boolean());
+
+Deno.test(`should pass either wrapped or "undefined" values`, () => {
+  const correct = [true, false, undefined];
+  const incorrect = ["hello", 1234, null, [], {}];
+
+  for (const example of correct) {
+    assertEquals(schema.check(example, context), example);
+  }
+
+  for (const example of incorrect) {
+    assertIsError(schema.check(example, context));
+  }
+});
