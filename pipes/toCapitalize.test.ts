de13bee355ec9fab6b97663a370ba1ea77bc7bc0
Andres Celis <adwher@icloud.com>
2023-10-22 18:14:08 -0500

    feat(ci): unit-tests for `toCapitalize()`

---
+++ pipes/toCapitalize.test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { createContext } from "../context.ts";
+import { pipe, string } from "../schemas/mod.ts";
+import { toCapitalize } from "./toCapitalize.ts";
+
+const context = createContext();
+const schema = pipe(string(), toCapitalize());
+
+Deno.test("should capitalize the given text", () => {
+  const examples = [
+    ["hello", "Hello"],
+    ["joe doe", "Joe Doe"],
+    ["the new york times", "The New York Times"],
+    ["must be capitalized", "Must Be Capitalized"],
+  ];
+
+  for (const [example, capitalized] of examples) {
+    assertEquals(schema.check(example, context), capitalized);
+  }
+});
