ab98a976494256efa3e681b3f1284090c89a6e35
Andres Celis <adwher@icloud.com>
2023-10-22 20:16:09 -0500

    feat(ci): unit-tests for `partial()`

---
+++ schemas/partial.test.ts
@@ -0,0 +1,20 @@
+import { assertIsError, assertObjectMatch } from "std/assert/mod.ts";
+import { object, partial, string } from "./mod.ts";
+import { createContext } from "../context.ts";
+
+const context = createContext();
+
+Deno.test("should strict the schema shape", () => {
+  const schema = partial(object({ hello: string() }));
+
+  const correct = [{ hello: undefined }, { hello: "word!" }];
+  const incorrect = [{ hello: 1234 }, { hello: null }];
+
+  for (const example of correct) {
+    assertObjectMatch(schema.check(example, context), { ...example });
+  }
+
+  for (const example of incorrect) {
+    assertIsError(schema.check(example, context));
+  }
+});
