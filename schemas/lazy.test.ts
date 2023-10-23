9628f3ded95fe7f7a26e88e79b13585d505d5107
Andres Celis <adwher@icloud.com>
2023-10-22 19:27:24 -0500

    feat(ci): unit-tests for `lazy()`

---
+++ schemas/lazy.test.ts
@@ -0,0 +1,30 @@
+import { assertIsError, assertObjectMatch } from "std/assert/mod.ts";
+import { Describe } from "../schema.ts";
+import { array, lazy, object } from "./mod.ts";
+import { createContext } from "../context.ts";
+import { createError } from "../errors.ts";
+
+const context = createContext();
+
+Deno.test("should return given errors", () => {
+  const schema = lazy(() => createError(context));
+
+  const examples = ["hello", 1234, true, false, null, [], {}];
+
+  for (const example of examples) {
+    assertIsError(schema.check(example, context));
+  }
+});
+
+Deno.test("should allow nested schemas", () => {
+  type Node = { children: Node[] };
+
+  const schema: Describe<Node> = lazy(() =>
+    object({ children: array(schema) })
+  );
+
+  const received = { children: [{ children: [] }] };
+  const expected = { children: [{ children: [] }] };
+
+  assertObjectMatch(schema.check(received, context), expected);
+});
