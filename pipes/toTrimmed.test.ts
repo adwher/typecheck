c74540fec2d76a99edcf9ab29942b89a82e8a894
Andres Celis <adwher@icloud.com>
2023-10-24 21:59:04 -0500

    feat(ci): unit-tests for `toTrimmed()`

---
+++ pipes/toTrimmed.test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { createContext } from "../context.ts";
+import { pipe, string } from "../schemas/mod.ts";
+import { toTrimmed } from "./toTrimmed.ts";
+
+const context = createContext();
+const schema = pipe(string(), toTrimmed());
+
+Deno.test("should remove white spaces", () => {
+  const examples = [
+    [" one", "one"],
+    ["one ", "one"],
+    ["two  ", "two"],
+    ["  two", "two"],
+  ];
+
+  for (const [example, trimmed] of examples) {
+    assertEquals(schema.check(example, context), trimmed);
+  }
+});
