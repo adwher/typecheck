2b5971645f9e9ab1a28ccb559ba6ae02b2b12b5d
Andres Celis <adwher@icloud.com>
2023-10-24 22:02:11 -0500

    feat(ci): unit-test for `toLowerCase`

---
+++ pipes/toLowerCase.test.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { createContext } from "../context.ts";
+import { pipe, string } from "../schemas/mod.ts";
+import { toLowerCase } from "./toLowerCase.ts";
+
+const context = createContext();
+const schema = pipe(string(), toLowerCase());
+
+Deno.test("should replace with lower case", () => {
+  const examples = [
+    ["bye", "bye"],
+    ["HELLO", "hello"],
+    ["Joe Doe", "joe doe"],
+    ["The New YORK Times", "the new york times"],
+    ["MUST BE IN LOWER CASE", "must be in lower case"],
+  ];
+
+  for (const [example, lowercase] of examples) {
+    assertEquals(schema.check(example, context), lowercase);
+  }
+});
