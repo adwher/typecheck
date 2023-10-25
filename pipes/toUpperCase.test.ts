15ad65bb3be187f47bbc1def502f4a7c0e27c66f
Andres Celis <adwher@icloud.com>
2023-10-24 22:03:46 -0500

    feat(ci): unit-tests for `toUpperCase()`

---
+++ pipes/toUpperCase.test.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { createContext } from "../context.ts";
+import { pipe, string } from "../schemas/mod.ts";
+import { toUpperCase } from "./toUpperCase.ts";
+
+const context = createContext();
+const schema = pipe(string(), toUpperCase());
+
+Deno.test("should replace with upper case", () => {
+  const examples = [
+    ["BYE", "BYE"],
+    ["hello", "HELLO"],
+    ["Joe Doe", "JOE DOE"],
+    ["The New YORK Times", "THE NEW YORK TIMES"],
+    ["must be in upper case", "MUST BE IN UPPER CASE"],
+  ];
+
+  for (const [example, lowercase] of examples) {
+    assertEquals(schema.check(example, context), lowercase);
+  }
+});
