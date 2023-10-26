ddd8ba9535802c5ed6c2e75ee425fde465da78fc
Andres Celis <adwher@icloud.com>
2023-10-25 21:47:55 -0500

    feat(ci): unit-tests for `object()`

---
+++ schemas/object.test.ts
@@ -0,0 +1,60 @@
+import {
+  assertEquals,
+  assertIsError,
+  assertObjectMatch,
+} from "std/assert/mod.ts";
+import { createContext } from "../context.ts";
+import { number, object, string } from "./mod.ts";
+
+const context = createContext();
+
+Deno.test("allows object values", () => {
+  const schema = object({ a: string() });
+
+  const correct = [{ a: "hello" }, { a: "world" }];
+  const incorrect = ["hello", 1234, null, false, true, [], {}];
+
+  for (const example of correct) {
+    assertEquals(schema.check(example, context), example);
+  }
+
+  for (const example of incorrect) {
+    assertIsError(schema.check(example, context));
+  }
+});
+
+Deno.test("respect the given shape", () => {
+  const schema = object({
+    title: string(),
+    description: string(),
+    reads: number(),
+  });
+
+  const correct = [
+    { title: "Post #1", description: "Lorem", reads: 1 },
+    { title: "Post #2", description: "Lorem", reads: 2 },
+    { title: "Post #3", description: "Lorem", reads: 3 },
+  ];
+
+  const incorrect = [
+    { title: "Post #1", description: "Lorem", reads: true },
+    { title: "Post #1", description: null, reads: 10 },
+    { title: [], description: "Lorem", reads: 10 },
+  ];
+
+  for (const example of correct) {
+    assertEquals(schema.check(example, context), example);
+  }
+
+  for (const example of incorrect) {
+    assertIsError(schema.check(example, context));
+  }
+});
+
+Deno.test("return an issue with the right path", () => {
+  const schema = object({ a: string() });
+  const output = schema.check({ a: null }, context);
+
+  assertIsError(output);
+  assertObjectMatch(output.first(), { path: ["a"] });
+});
